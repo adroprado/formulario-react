1c68aa307b27f99162261ebc4a7b105153a60f78
Alejandro Prado <adro.viprado@gmail.com>
2025-12-08 08:53:56 -0600

    feat(evento): se añade evento onSubmit y onChange con su respectiva lógica

--- src/componentes/ValidacionesDeFormulario.jsx
+++ src/componentes/ValidacionesDeFormulario.jsx
@@ -1,11 +1,47 @@
+import { useState } from "react";
+
+const contactoInicial = {
+  nombre: "",
+  correo: "",
+  asunto: "",
+  comentarios: "",
+  // Estado para controlar los errores
+  errores: {
+    nombre: null, // null (o false) si es válido, string con mensaje si es inválido
+    correo: null,
+    asunto: null,
+    comentarios: null,
+  },
+  carga: false, // para el loader
+  mensaje: null, // para la respuesta simulada del servidor
+};
+
 const ValidacionesDeFormulario = () => {
+  const [formulario, setFormulario] = useState(contactoInicial);
+
+  // --- Manejando datos de estado ---
+  const manejarCambios = (e) => {
+    setFormulario({
+      ...formulario, // 1. Copia inmutable del estado actual
+      [e.target.name]: e.target.value, // 2. Actualiza SOLO el campo que disparó el evento (usando el atributo 'name')
+    });
+  };
+
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main>
       <a href="https://github.com/adroprado/formulario-react" target="_blank">
         <img src="/src/assets/github.svg" alt="Github" />
       </a>
       <h2>Validaciones de Formulario</h2>
-      <form className="formulario-contacto" autoComplete="off">
+      <form
+        className="formulario-contacto"
+        autoComplete="off"
+        onSubmit={manejarEnvio}
+      >
         <legend>Envianos tus comentarios</legend>
         <input
           type="text"
@@ -14,6 +50,8 @@ const ValidacionesDeFormulario = () => {
           title="Nombre sólo acepta letras y espacios en blanco"
           pattern="^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$"
           required
+          value={formulario.nombre}
+          onChange={manejarCambios}
         />
         {/* Podemos trabajar expresiones regulares desde html, para ello usamos el atributo "pattern" */}
         <input
@@ -23,6 +61,8 @@ const ValidacionesDeFormulario = () => {
           title="Email incorrecto"
           pattern="^[a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,15})$"
           required
+          value={formulario.correo}
+          onChange={manejarCambios}
         />
         <input
           type="text"
@@ -30,6 +70,8 @@ const ValidacionesDeFormulario = () => {
           placeholder="Asunto a tratar"
           title="El asunto es requerido"
           required
+          value={formulario.asunto}
+          onChange={manejarCambios}
         />
         {/* Los textarea no aceptan un pattern cómo los input, pero usaremos un atributo data, y con jS validaremos este atributo */}
         <textarea
@@ -40,16 +82,30 @@ const ValidacionesDeFormulario = () => {
           title="Tu comentario no debe exceder los 255 caracteres"
           data-pattern="^.{1,255}$"
           required
+          value={formulario.comentarios}
+          onChange={manejarCambios}
         ></textarea>
         <input type="submit" value="Enviar" />
-        <div className="formulario-contacto-cargador none">
-          <img src="assets/oval.svg" alt="Cargando" />
-        </div>
-        <div className="formulario-contacto-respuesta none">
-          <p>Los datos han sido enviados</p>
-        </div>
       </form>
     </main>
+  );
+};
+
+const CargadorDeEspera = () => {
+  return (
+    <>
+      <div className="formulario-contacto-cargador none">
+        <img src="assets/oval.svg" alt="Cargando" />
+      </div>
+    </>
+  );
+};
+
+const Mensaje = () => {
+  return (
+    <div className="formulario-contacto-respuesta none">
+      <p>Los datos han sido enviados</p>
+    </div>
   );
 };
 
